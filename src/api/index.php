1db1badf2aaabfb93f67784d0918884cc4ddc7a2
Paulo Martins <paulovelho@paulovelho.com>
2024-04-11 18:31:09 -0300

    debugging prod

--- src/api/index.php
+++ src/api/index.php
@@ -3,6 +3,11 @@
 include("_inc.php");
 include("api.php");
 
+use Magrathea2\Debugger;
+Debugger::Instance()
+	->SetType(Debugger::LOG)
+	->LogQueries(false);
+
 $api = new MagratheaCloud\MagratheaCloudApi();
 
 if(@$_GET["debug"] == "true") {
